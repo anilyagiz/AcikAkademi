99e4c3a9b3edd960d7abb61e011951cf04c08f8e
anilyagiz <anill.yagiz@gmail.com>
2023-04-12 01:42:41 +0300

    0
    0

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Açık Akademi</span>,
+  project: {
+    link: 'https://github.com/anilyagiz',
+  },
+  chat: {
+    link: 'https://discord.gg/wZh2HgGDZV',
+  },
+  docsRepositoryBase: 'https://github.com/anilyagiz',
+  footer: {
+    text: 'Açık Akademi, eğitimde eşitlik sağlamak amacıyla kurulmuş kar amacı gütmeyen bir platformdur. ',
+  },
+}
+
+export default config
